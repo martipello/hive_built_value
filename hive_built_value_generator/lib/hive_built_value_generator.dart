070db02617879b3f1f0df56306d21ee48cef23ba
martin seal <martin@wordskii.com>
2022-05-26 19:50:19 +0100

    FIX builder generator

--- hive_built_value_generator/lib/hive_built_value_generator.dart
+++ hive_built_value_generator/lib/hive_built_value_generator.dart
@@ -3,4 +3,4 @@ import 'package:hive_built_value_generator/src/type_adapter_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
 Builder getBuilder(BuilderOptions options) =>
-    SharedPartBuilder([TypeAdapterGenerator()], 'hive_generator');
+    SharedPartBuilder([TypeAdapterGenerator()], 'hive_built_value_generator');
